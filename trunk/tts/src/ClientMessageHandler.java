c00c5ffc4356dc4540bc58650c4c0fc8a8884703
travis.yoo <travis.yoo@5c20b09b-a547-0410-a0b9-d7e9a796042d>
2008-03-22 07:56:36 +0000

    3/22
    1) Implement an connection between the server and device
    
    2) Get inputstream from the server, and pass it to the XML parser

---
+++ trunk/tts/src/ClientMessageHandler.java
@@ -0,0 +1,48 @@
+//import java.io.*;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+package talkingPoints;
+
+public class ClientMessageHandler {
+	 
+	ClientDataModel clientDataModel = new ClientDataModel();  //changing 
+		
+	public ClientMessageHandler(){
+		
+	}
+	
+	public void tagWasRead(long tagID) throws Exception{
+		// for test, use absolute URL address
+		URL url = new URL("http://grocs.dmc.dc.umich.edu:3000/locations/show_by_bluetooth_mac");
+		
+		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+		conn.setDoInput(true);
+		conn.setDoOutput(true);
+		conn.setRequestMethod("GET");
+		conn.setUseCaches(false);
+		conn.setRequestProperty("Content-Type","text/xml");
+		//conn.connect();
+		
+		InputStream in = conn.getInputStream(); // InputStream for espresso-royale.xml
+		
+		// in real life, we will send a message to the backend (TP2S)
+
+		System.out.println("Tag was read: " + tagID);
+		if(tagID == 123)
+		{
+			clientDataModel.parseXML(in);
+			clientDataModel.tagChecking(123);
+		}
+		
+		conn.disconnect();
+		/*
+		if(tagID == 124)
+			clientDataModel.parseXML("stucchis.xml"); // no later
+			*/
+		//clientDataModel.tagChecking(tagID); // just for test, pass tagID to tagChecking
+	}
+
+	
+}
---
+++ trunk/tts/src/TagReaderTest.java
@@ -0,0 +1,26 @@
+//package talkingPoints;
+
+import java.io.IOException;
+
+
+public class TagReaderTest {
+
+	// Please note: this will never die. You'll have to kill it manually.
+	public static void main (String args[]){
+		ClientMessageHandler clientMessageHandler = new ClientMessageHandler(); // also loading xml file
+		final TagReader tagReader = new TagReader(clientMessageHandler);
+		final java.util.Timer timer = new java.util.Timer();
+		java.util.TimerTask timerTask = new java.util.TimerTask() {
+			public void run() {
+				//try{
+					//tagReader.bluetoothSearch();				// search Bluetooth MacAddress
+					tagReader.generateFakeTagEvent(); // for Tag Test
+				//}catch(IOException e){
+					
+				//}	
+			}
+		};
+		// will fire a new TimerTask every 5 sec, each of which will generate a fake tag event
+		timer.scheduleAtFixedRate(timerTask, 0, 1000);
+	}	
+}
