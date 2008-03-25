4cd192e9b8ed2473c3c4912899e7c7ee0deb61e6
peter.kretschman <peter.kretschman@5c20b09b-a547-0410-a0b9-d7e9a796042d>
2008-03-25 04:56:06 +0000

    Added ability to press buttons appearing in locationList, and corresponding action listener.  Added preliminary "more information" page, and configured TP logo button to return to main page.

--- trunk/tts/src/TalkingPointsGUI.java
+++ trunk/tts/src/TalkingPointsGUI.java
@@ -2,7 +2,7 @@
  * 
  */
 
-
+package talkingPoints;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -19,12 +19,19 @@ import javax.swing.JViewport;
 import javax.swing.ImageIcon;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.UIManager;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JCheckBox;
+import javax.swing.event.TableModelListener;
+import javax.swing.event.TableModelEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.BorderLayout;
 
-public class TalkingPointsGUI  {
+public class TalkingPointsGUI implements ActionListener {
 
 	// Constants
 	static final int COMP_SPACER_X = 10;
@@ -32,7 +39,9 @@ public class TalkingPointsGUI  {
 	static final int TOP_SPACER_X = 50;
 	static final int VIEWALL_X = 80;
 	static final int VIEWALL_Y = 45;
-		
+	static final String MAINPANE = "Front pane of TPGUI";
+	static final String MOREINFO = "More information pane of TPGUI";
+	
 	// Default constructor
 	TalkingPointsGUI()  {
 		System.out.println("Object " + this.toString() + " created.");
@@ -48,7 +57,7 @@ public class TalkingPointsGUI  {
 	// Does what it says; initialize the GUI and its components
 	private void initGUI() {
 			
-		// Allocate components for the GUI's front page
+		// Allocate components for the GUI's pages
 		ImageIcon forward = createImageIcon("images/forward.png", "Button that advances to a screen with more information about this point of interest.");
 		
 		mainFrame = new JFrame("Talking Points");
@@ -61,7 +70,10 @@ public class TalkingPointsGUI  {
 		legend = new JLabel(createImageIcon("images/legend.png", "Legend for this page's icons."),
 				javax.swing.SwingConstants.CENTER);
 		tableTitle = new JLabel("<html><font size = 4><u>Detected Points of Interest</font></u></html>", javax.swing.SwingConstants.CENTER);
-				
+		centralPane = new JPanel(new CardLayout());
+		locationName = new JLabel("Empty");
+		locationDesc = new JLabel("Empty");
+		
 		// Pre-calculate our desired component sizes to save time/computation
 		int forwardWidth = forward.getIconWidth() ;
 		int forwardHeight = forward.getIconHeight();
@@ -96,8 +108,10 @@ public class TalkingPointsGUI  {
 		frontScroll.setMaximumSize(new Dimension((windowWidth - COMP_SPACER_X - COMP_SPACER_X), tableHeight));
 		
 		// Configure location list table
+		ourModel.addTableModelListener(new mainModelListener());
 		locationList.setModel(ourModel);
 		locationList.getColumnModel().getColumn(0).setCellRenderer(new ButtonRenderer());
+		locationList.getColumnModel().getColumn(0).setCellEditor(new ButtonEditor(new JCheckBox()));
 		locationList.getColumnModel().getColumn(0).setPreferredWidth(forwardWidth);
 		locationList.getColumnModel().getColumn(0).setMinWidth(forwardWidth);
 		locationList.getColumnModel().getColumn(0).setMaxWidth(forwardWidth);
@@ -129,9 +143,18 @@ public class TalkingPointsGUI  {
 		logoButton.setPreferredSize(new Dimension(logoWidth, logoHeight));
 		logoButton.setMinimumSize(new Dimension(logoWidth, logoHeight));
 		logoButton.setMaximumSize(new Dimension(logoWidth, logoHeight));
+		logoButton.setActionCommand("home");
+		logoButton.addActionListener(this);
 
 		// Configure View All button
 		viewAll.setPreferredSize(new Dimension(VIEWALL_X, VIEWALL_Y));		
+		viewAll.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		viewAll.setAlignmentY(Component.CENTER_ALIGNMENT);
+		
+		// CONFIGURE MORE INFORMATION PANEL
+		// Configure title labels
+		locationName.setPreferredSize(new Dimension(100, 15));
+		locationDesc.setPreferredSize(new Dimension(100, 15));
 		
 		// Set up horizontal layer at top of window
 		JPanel topButtons = new JPanel();
@@ -149,18 +172,37 @@ public class TalkingPointsGUI  {
 		middlePane.add(Box.createRigidArea(new Dimension(COMP_SPACER_X, (COMP_SPACER_Y / 2))));
 		middlePane.add(frontScroll);
 		middlePane.add(Box.createRigidArea(new Dimension(COMP_SPACER_X, (COMP_SPACER_Y / 2))));
-						
+		middlePane.add(viewAll);
+		middlePane.add(Box.createRigidArea(new Dimension(COMP_SPACER_X, COMP_SPACER_Y)));
+		
 		/// Set up final horizontal layer of only the view all button
 		JPanel bottomButton = new JPanel();
 		bottomButton.setLayout(new BoxLayout(bottomButton, BoxLayout.LINE_AXIS));
 		bottomButton.add(Box.createGlue());
 		bottomButton.add(viewAll);
-		bottomButton.add(Box.createRigidArea(new Dimension(COMP_SPACER_X, COMP_SPACER_Y)));
+		bottomButton.add(Box.createRigidArea(new Dimension(COMP_SPACER_X, COMP_SPACER_Y))); 
+		middlePane.add(bottomButton);
+		centralPane.add(middlePane, MAINPANE);
+		
+		// Set up More Information pane
+		JPanel moreInfo = new JPanel();
+		
+		// Set up title bar
+		JPanel title = new JPanel();
+		title.setLayout(new BoxLayout(title, BoxLayout.X_AXIS));
+		title.setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0)));
+		title.setBackground(new Color(172,193,174));
+		title.add(locationName);
+		title.add(Box.createRigidArea(new Dimension(COMP_SPACER_X, COMP_SPACER_Y)));
+		title.add(locationDesc);
+		
+		moreInfo.add(title);
+		
+		centralPane.add(moreInfo, MOREINFO);
 		
 		// Add components to main content pane
 		mainContentPane.add(topButtons, BorderLayout.PAGE_START);
-		mainContentPane.add(middlePane, BorderLayout.CENTER);
-		mainContentPane.add(bottomButton, BorderLayout.PAGE_END);
+		mainContentPane.add(centralPane, BorderLayout.CENTER);
 		mainFrame.setContentPane(mainContentPane);
 		mainFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
 		
@@ -179,21 +221,40 @@ public class TalkingPointsGUI  {
 		}
 	}
 	
+	// Listener for the table model that is assigned to the main pane table
+	// TODO: In case the table gets changed, we need to make a copy of the data while we're looking at its details
+	class mainModelListener implements TableModelListener {
+		
+		public void tableChanged(TableModelEvent evt) {
+		
+			if(evt.getColumn() == 123) {
+				int row = evt.getFirstRow();
+				locListModel model = (locListModel)locationList.getModel();
+				locationName.setText((String)model.getValueAt(row, 1));
+				locationDesc.setText((String)model.getValueAt(row, 2));
+				CardLayout cl = (CardLayout)centralPane.getLayout();
+				cl.show(centralPane, MOREINFO);
+			}
+		}
+		
+	}
+	
+	// Listener for the Talking Points logo button (may expand to cover other types of buttons if needed)
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand() == "home")  {
+		CardLayout cl = (CardLayout)centralPane.getLayout();
+		cl.show(centralPane, MAINPANE);
+		}
+	}
+	
 	/**
 	 * @param args is unused
-	 */ /*
+	 */  /*
 	public static void main(String[] args) {
 		TalkingPointsGUI ourGUI = new TalkingPointsGUI();
 		
-		POIdata test = new POIdata("things", "place", "stuff");
+		ourGUI.addItem(new POIdata("things", "place", "stuff", "words", "bleh", "duder", "blah", "schmelding", "potrzebie", "arglglg"));
 		System.out.println("Adding item");
-		POIdata test2 = new POIdata("what", "huh", "This is a really long field.  I mean REALLY loooooooong.  We need to be able to read all of it.");
-		ourGUI.addItem(test);
-		ourGUI.addItem(test2);
-		ourGUI.addItem(test);
-		ourGUI.addItem(test);
-		ourGUI.addItem(test);
-		ourGUI.addItem(test2);
 	}  */
 
 	/* Custom table model for locationList.
@@ -218,6 +279,14 @@ public class TalkingPointsGUI  {
 			return 3;
 		}
 		
+		// Cells in column 0 must be "editable" in order to be clickable; other cells are not
+		public boolean isCellEditable(int rowIndex, int columnIndex) {
+			if(columnIndex == 0)
+				return true;
+			else
+				return false;
+		}
+		
 		public String getColumnName(int col) {
 			if(col == 0)
 				return("Icon");
@@ -230,7 +299,6 @@ public class TalkingPointsGUI  {
 		}
 		
 		// Required method getValueAt()
-		// TODO: Update if number of columns changes
 		public Object getValueAt(int row, int column) {
 			
 			if(data[row] == null)
@@ -240,7 +308,7 @@ public class TalkingPointsGUI  {
 				return data[row].name();
 			else if (column == 2)
 				return data[row].description();
-			else 
+			else
 				return forward;
 		}
 		
@@ -258,10 +326,8 @@ public class TalkingPointsGUI  {
 		/* does this work instead of creating a new POIdata object every time*/ 
 		data[0] = p;
 		//data[0] = new POIdata(p.name(), p.location_type(), p.description()); ;
-		
-		fireTableDataChanged();
-			
 		}
+		
 		
 		// Queue to store location data; this will consist of two strings contained in the class TableEntry.
 		private POIdata[] data;
@@ -293,22 +359,68 @@ public class TalkingPointsGUI  {
 				setIcon(null);
 				setEnabled(false);
 			}
-
 			return this;
 			
 		}
 	}
 	
-	// TODO: Add editor that will allow user to click on forward buttons
+	// Custom editor that allows buttons in table to be clicked
+	class ButtonEditor extends DefaultCellEditor
+					   implements ActionListener {
+		
+		ButtonEditor(JCheckBox checkBox) {
+			super(checkBox);
+			System.out.println(this + "created.");
+			button = new JButton();
+			button.setOpaque(true);
+			button.addActionListener(this);
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			fireEditingStopped();
+		}
+		
+		public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+			isPushed = true;
+			locListModel model = (locListModel)table.getModel();
+			
+			model.fireTableChanged(new TableModelEvent(model, row, row, 123));
+			return button;
+		}
+		
+		public Object GetCellEditorValue() {
+			if(isPushed)
+				System.out.println("Button pushed.");
+			isPushed = false;
+			return new String("Empty");
+		}
+		
+		public boolean StopCellEditing() {
+			System.out.println("Editing stopped.");
+			isPushed = false;
+			return super.stopCellEditing();
+		}
+		
+		protected void fireEditingStopped() {
+			super.fireEditingStopped();
+		}
+		
+		// Variable declarations
+		protected JButton button;
+		private boolean isPushed;
+	}
 	
 	// Variable definitions
 	private locListModel ourModel;
 	private JFrame mainFrame;
 	private JPanel mainContentPane;
+	private JPanel centralPane;
 	private JScrollPane frontScroll;
 	private JTable locationList;
 	private JButton logoButton;
 	private JButton viewAll;
 	private JLabel legend;
 	private JLabel tableTitle;
+	private JLabel locationName;
+	private JLabel locationDesc;
 }
