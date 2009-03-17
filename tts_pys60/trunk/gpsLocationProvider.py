9c8567e43f66bd2f6756f42f7b3f1b887fa3e3db
peter.kretschman <peter.kretschman@5c20b09b-a547-0410-a0b9-d7e9a796042d>
2009-03-17 18:27:50 +0000

    gpsLocationProvider is now multithreaded!

--- tts_pys60/trunk/gpsLocationProvider.py
+++ tts_pys60/trunk/gpsLocationProvider.py
@@ -1,41 +1,45 @@
 #GPS Reader
-#set up a server here, and send dummy data via the port 30940
 import appuifw
 import e32
 import urllib
-import time
-import socket
+import thread
 import positioning
 import math
 import string
+import time
 import sys
 sys.path.append('e:\python\libs')
 import json
+sys.path.append('e:\python')
+sys.path.append('C:\python')
+import serverAPI
 
+keep_scanning = True
+
+#TODO: put this in a separate thread
 class GpsLocProvider:
     
     def __init__(self, neardist, fardist):
-        self.HOST = '127.0.0.1'
-        self.BACKEND_PORT = 30940
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.currentLoc = { }
         self.nearbyPOIs = [ ] #should be a list of dictionaries
         self.nearTolerance = neardist #how close we need to be to a POI to trigger it
         self.farTolerance = fardist #how far we must be from a POI before it's removed from the recent list
         # Initialize GPS module here
-        positioning.select_module(positioning.default_module())
-        positioning.set_requestors([{"type":"service", 
-                             "format":"application",
-                             "data":"GpsLocProvider"}])    
-   
-    def generateDummyGPSData(self):
-        s.bind((self.HOST, BACKEND_PORT))
-        s.listen(1)
+        self.server = serverAPI.ServerAPI()
+        #positioning.select_module(positioning.default_module())
+        #positioning.set_requestors([{"type":"service", 
+        #                     "format":"application",
+        #                     "data":"GpsLocProvider"}])    
         
-        conn, addr = s.accept()
-        while 1:
-            conn.send({"lat":-12.333, "lng":22.333333})
-            time.sleep(5)
+        self.nearbyLock = thread.allocate_lock()
+        self.updateThread = thread.start_new_thread(self.Update, () )
+        #self.updateThread = thread(group=None, target=self.Update(), name="GPS Update Thread")
+        #self.updateThread.setDaemon(true)
+    
+    def exit_thread(self):
+        global keep_scanning
+        keep_scanning = False
+    
     
     def getCurrentLocation(self):
         print "Querying position module..."
@@ -45,9 +49,11 @@ class GpsLocProvider:
         print self.currentLoc
         return self.currentLoc
     
+    
     # incPOIs: list of new POIs (which are dictionaries)
     def addPOIs(self, incPOIs): 
         # Check for duplicates
+        self.nearbyLock.acquire()
         try:
             if len(incPOIs) != 0: #len generates a TypeError exception
                 for poi in incPOIs: #poi is a key
@@ -55,58 +61,63 @@ class GpsLocProvider:
                     if newDict not in self.nearbyPOIs:
                         self.nearbyPOIs.append(newDict) 
         except TypeError:
+            self.nearbyLock.release()
             return
         
-    # 
-    def Update(self):
-        initialTime = time.clock()
-        activeList = [ ] 
-        # get current location, compare to old current location
-        newLoc = self.getCurrentLocation()
-        # if no(or little) change, don't do anything
-        latDif = math.fabs(newLoc["lat"] - self.currentLoc["lat"])
-        lngDif = math.fabs(newLoc["lng"] - self.currentLoc["lng"])
-       # if latDif < 0.0001 or lngDif < .001:
-       #     print "Haven't moved far enough.  Cancelling update."
-       #     return activeList
-       # else:
-        print "Updated current location."
-        self.currentLoc = newLoc
-        self.addPOIs(self.get_nearby_locations(self.currentLoc["lat"], self.currentLoc["lng"]))
+        self.nearbyLock.release()
         
+    # Performs a full update of the object 
+    def Update(self):
+        positioning.select_module(positioning.default_module())
+        positioning.set_requestors([{"type":"service", 
+                             "format":"application",
+                             "data":"GpsLocProvider"}])  
+        global keep_scanning
+        
+        while keep_scanning:
+            initialTime = time.clock() 
+            # get current location, compare to old current location
+            newLoc = self.getCurrentLocation()
+            # if no(or little) change, don't do anything
+            latDif = math.fabs(newLoc["lat"] - self.currentLoc["lat"])
+            lngDif = math.fabs(newLoc["lng"] - self.currentLoc["lng"])
+            # if latDif < 0.0001 or lngDif < .001:
+            #     print "Haven't moved far enough.  Cancelling update."
+            #     return activeList
+            # else:
+            print "Updated current location."
+            self.currentLoc = newLoc
+            
+            #self.nearbyLock.signal()
+            self.addPOIs(self.get_nearby_locations(self.currentLoc["lat"], self.currentLoc["lng"]))
+                
+            print "Update in :" + str(time.clock() - initialTime) + " ms."       
+            e32.ao_yield()
+            e32.ao_sleep(8)
+        
+        print "Update thread done."
+            
+    # returns list of dictionaries that contain full POI data
+    def get_active_list(self):
+        activeList = [ ]
+        
+        self.nearbyLock.acquire()
         for poi in self.nearbyPOIs:
             for key in poi:
                 tempDict = poi[key]
                 dist = self.calcDistance(self.currentLoc, tempDict) #poi must have "lat" and "lng" entries
                 if dist <= self.nearTolerance:
-                    activeList.append(poi)
+                    activeList.append(self.server.get_location(tempDict['tpid']))
                 if dist >= self.farTolerance:
                     self.nearbyPOIs.remove(poi)
-        
-        print "Update in :" + str(time.clock() - initialTime) + " ms."       
+        self.nearbyLock.release()
         return activeList
-    
-    def get_nearby_locations(self, lat, lng):
+                    
+                    
+    def get_nearby_locations(self, lat, lng): 
         print "Contacting remote server..."
-        self.server_host = "http://test.talking-points.org"
-        latstr = str(lat)
-        lngstr = str(lng)
-        lat = string.replace(latstr, '.', ',')
-        lng = string.replace(lngstr, '.', ',') 
-        request_url = self.server_host + "/locations/get_nearby/" + lat + ";" + lng + ".json"
-        try:
-            response = urllib.urlopen(request_url).read()
-            print "Got response."
-            processed = json.read(response)
-            if len(processed) == 0:
-                print "Didn't find anything."
-            else:
-                print "Found " + str(len(processed)) + " POIs."
-            return processed    
-        except IOError:
-            print "Connection failed."
-            return
-        
+        return self.server.get_nearby_locations(lat, lng)
+  
         
     # pointA and pointB must be dictionaries with 'lat' and 'lng' entries
     def calcDistance(self, pointA, pointB):
@@ -134,14 +145,28 @@ class GpsLocProvider:
         return distInMiles
     
 # test code begins here    
-GPS = GpsLocProvider(1.1, 2)
-while 1:
-    actives = GPS.Update()
-    print "got " + str(len(actives)) + " POIs."
+repeat = 0
+GPS = GpsLocProvider(1.1, 3.0)
+while repeat < 10:
+    repeat += 1
+    print "repetition #" + str(repeat)
+    actives = GPS.get_active_list()
     if len(actives) != 0:
         for poi in actives:
-            for key in poi:
-                notestring = (u"You are getting close to TPID " + str(poi[key]["tpid"]))
-                appuifw.note(notestring, "info")
+            notestring = (u"You are getting close to TPID " + str(poi['name']))
+            appuifw.note(notestring, "info")
     e32.ao_yield()
-    e32.ao_sleep(8)
+    e32.ao_sleep(10)
+
+GPS.exit_thread()
+
+#GPS = GpsLocProvider(1.1, 0.00000001)
+#while 1:
+#    actives = GPS.Update()
+#    print "got " + str(len(actives)) + " POIs."
+#    if len(actives) != 0:
+#        for poi in actives:
+#            notestring = (u"You are getting close to TPID " + str(poi['name']))
+#            appuifw.note(notestring, "info")
+#    e32.ao_yield()
+#    e32.ao_sleep(8)
