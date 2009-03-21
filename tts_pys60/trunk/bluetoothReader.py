60f17973030dbb0a0f0f1db12fe43c5896d5e495
travis.yoo <travis.yoo@5c20b09b-a547-0410-a0b9-d7e9a796042d>
2009-03-21 01:04:30 +0000

    BluetoothReader Recent Working Version

--- tts_pys60/trunk/bluetoothReader.py
+++ tts_pys60/trunk/bluetoothReader.py
@@ -26,23 +26,30 @@ class BluetoothReader:
 		
 	def notifyMacAddress(self, macAddress):
 		global detected_locations_dic
-		#callback function here
-		if(macAddress == "001ff3b01a1e"):
-			try:
-				#see if this mac address has been detected macaddress
-				#This needs to be modified
-				detected_locations_dic.index(macAddress)
-			except ValueError: #if this is not in the dictionary
+		flag = 0
+		if(macAddress == "001ff3b01a1e"): #for test
+			#see if this mac address has been detected
+			for i,v in detected_locations_dic.iteritems():
+				if v['bluetooth_mac'] == str(macAddress): #if this has been detected
+					flag = 1
+					break
+						
+			if flag == 0 :
 				#Connect the server and get the information
+				json_parsed = {}
+				json_parsed['name'] = 'Espresso Royale'
+				json_parsed['tpid'] = '1'
+				json_parsed['bluetooth_mac'] = '001ff3b01a1e'
 				#Also, pass this dictionary to the UI
-				place_name = "Espresso Royale"
-				tp_server = "http://test.talking-points.org"
-				url = tp_server + "/locations/show/1.json"
-				json_src = urllib.urlopen(url).read()
-				json_parsed = json.read(json_src)
+				#json_parsed = sAPI.get_location_by_bluetooth_mac(macAddress) Hopefully this works?
+				#tp_server = "http://test.talking-points.org"
+				#url = tp_server + "/locations//1.json"
+				#json_src = urllib.urlopen(url).read()
+				#json_parsed = json.read(json_src)
 				appuifw.note(u"Place:" + str(json_parsed['name']), "info", 1) #this should be at the view class (global note)
-				detected_locations_dic.append(unicode(json_parsed['name'])) #append mac address here
-				tp_no_class_test.notifyMacAddress(detected_locations_list)
+				detected_locations_dic[json_parsed['tpid']] = json_parsed #update detected_locations list
+				#.append(unicode(json_parsed['name'])) #append mac address here
+				tp_no_class_test.notifyMacAddress(detected_locations_dic)
 				
 								
 	def __callback(self, error, mac, name, fp):
@@ -82,6 +89,8 @@ class BluetoothReader:
 			self.resolver.close()	
 			
 
+
 tp_no_class_test.uiStart() #ui start
+#sAPI = serverAPI.ServerAPI() #serverAPI start
 bluetoothReader = BluetoothReader()
 bluetoothReader.btSearch()
