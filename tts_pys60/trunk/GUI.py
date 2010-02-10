97bc40bb09c315da6b21f9610397ecbdc786df41
peter.kretschman <peter.kretschman@5c20b09b-a547-0410-a0b9-d7e9a796042d>
2010-02-10 23:15:42 +0000

    Updated GUI to use a monolithic drawGUI function, is always called by the main thread to avoid crashes.  Bookmark and Hide functions are currently broken.

--- tts_pys60/trunk/GUI.py
+++ tts_pys60/trunk/GUI.py
@@ -19,10 +19,15 @@ class GUI:
         self.location_listbox_mapping = {}
         self.location_menu_list = []
         self.location_menu_listbox = []
+        self.location_details_text = []
         
         self.comment_list = []
         self.notifyable = 1
         self.terminated = 0
+        
+        self.redraw_flag = 0
+        self.gui_state = "LocationList"
+        self.gui_state_stack = []
         
         self.current_tpid = None
         self.current_position = {} # will be updated by GPS
@@ -49,6 +54,139 @@ class GUI:
         txt.add( unicode(string) )
         return txt
 
+    # monolithic GUI update function, meant to be called by main thread
+    def drawGUI(self):
+        self.redraw_flag = 0
+        # This menu should be on all pages of the GUI, right?
+        appuifw.app.menu = [(u"Navigate", (
+                                (u"Where am I?", self.whereAmI),
+                                (u"Nearby", self.nearby),
+                                (u"Search", self.search))
+                            ), 
+                            (u"Bookmarked Locations", self.drawBookmarkedLocationList),
+                            (u"Hidden Locations", self.drawHiddenLocationList),
+                            (u"Settings", (
+                                (u"Input Mode", self.inputMode),
+                                (u"Font Size", self.fontSize))
+                            )]
+                            #seleting global menu by left softkey
+        appuifw2.app.menu_key_text = u"Menu"
+        
+        if self.gui_state == "LocationList":
+            self.notifyable = 1
+            self.current_tpid = None
+            appuifw.app.exit_key_handler = self.exit
+            appuifw2.app.exit_key_text = u"Exit"
+        
+            tmp = self.location_cache.getCurrentLocationList()
+            if len(tmp['list']) > 0:
+                location_list = tmp['list']
+                self.location_listbox_mapping = tmp['mapping']
+                self.location_listbox = appuifw.Listbox(
+                                                        location_list,
+                                                        self.locationListCallback
+                                                        )
+                appuifw.app.body = self.location_listbox
+            else:
+                appuifw.app.body = appuifw.Text( unicode("Searching ...") )
+        
+        elif self.gui_state == "LocationMenu":
+            appuifw.app.exit_key_handler = self.goBack
+            appuifw2.app.exit_key_text = u"Back"
+            self.location_menu_listbox = appuifw.Listbox(
+            self.location_menu_list,
+            self.locationMenuCallback
+        )
+            appuifw.app.body = self.location_menu_listbox
+        elif self.gui_state == "CommentsList":
+            if self.comment_list != None:
+                appuifw.app.exit_key_handler = self.goBack
+                appuifw2.app.exit_key_text = u"Back"
+                self.comment_listbox = appuifw.Listbox(self.comment_list, self.commentMenuCallback)
+                appuifw.app.body = self.comment_listbox
+            else:
+                appuifw.note(u"There are no comments yet.")
+                self.gui_state = self.gui_state_stack.pop()
+        elif self.gui_state == "CommentView":
+            appuifw2.app.body = self.location_details_text
+        elif self.gui_state == "Bookmark": 
+            msg = self.location_store.bookmark(self.current_tpid, self.location_cache.getLocationName(self.current_tpid))
+            appuifw.note(unicode(msg))
+            self.gui_state = self.gui_state_stack.pop()      
+        elif self.gui_state == "Hide":
+            msg = self.location_store.hide(self.current_tpid, self.location_cache.getLocationName(self.current_tpid))
+            appuifw.note(unicode(msg))
+            self.gui_state = self.gui_state_stack.pop() 
+        elif self.gui_state == "Details":
+            appuifw2.app.body = self.location_details_text
+        elif self.gui_state == "Bookmarks":
+            appuifw.app.body = self.bookmarked_listbox
+            appuifw.app.menu = [(u"Delete", self.deleteBookmark), (u"Select", self.bookmarkedListCallback)]
+    
+    def locationListCallback(self):
+        self.notifyable = 0
+        self.redraw_flag = 1
+        self.gui_state_stack.append(self.gui_state)
+        self.gui_state = "LocationMenu"
+        
+        if self.current_tpid == None:
+            self.current_tpid = self.location_listbox_mapping[self.location_listbox.current()]
+        
+        #self.drawLocationMenu()
+        tmp = self.location_cache.getLocationMenuList(self.current_tpid)
+        self.location_menu_list = tmp['list']
+        self.location_menu_mapping = tmp['mapping']
+        self.lock.signal()
+        
+    # sets up gui for detailed location display
+    def locationMenuCallback(self):
+        self.notifyable = 0
+        self.redraw_flag = 1
+        self.gui_state_stack.append(self.gui_state)
+        
+        menu_item_nr_selected = self.location_menu_listbox.current()
+        menu_item_text_selected = self.location_menu_mapping[menu_item_nr_selected]
+        
+        if menu_item_text_selected == "Comments":
+            self.gui_state = "CommentsList"
+            self.comment_list = self.location_cache.getLocationCommentsList(self.current_tpid)
+            self.lock.signal()
+        elif menu_item_text_selected == "Bookmark":
+            self.gui_state = "Bookmark"
+            self.lock.signal()
+        elif menu_item_text_selected == "Hide":
+            self.gui_state = "Hide"
+            self.lock.signal()
+        else:
+            self.gui_state = "Details"
+            self.location_details_text = appuifw2.Text_display(skinned=1, scrollbar=1, scroll_by_line=0)
+            self.location_details_text.add(menu_item_text_selected)
+            self.location_details_text.font = u"LatinBold19"
+            self.location_details_text.style = appuifw.STYLE_BOLD
+            self.lock.signal()
+            
+        #else:
+
+            #appuifw2.app.body = appuifw2.Text_display( menu_item_text_selected )
+         #   self.drawText( menu_item_text_selected )
+        
+    def commentMenuCallback(self):
+        self.notifyable = 0
+        self.redraw_flag = 1
+        self.gui_state_stack.append(self.gui_state)    
+        self.gui_state = "CommentView"
+        
+        comment_selected = self.comment_listbox.current()
+        self.location_details_text = appuifw2.Text_display(skinned=1, scrollbar=1, scroll_by_line=0)
+        self.location_details_text.add(self.comment_list[comment_selected])
+        self.location_details_text.font = u"LatinBold19"
+        self.location_details_text.style = appuifw.STYLE_BOLD
+        self.lock.signal()
+    
+    def goBack(self):
+        self.gui_state = self.gui_state_stack.pop()
+        self.redraw_flag = 1
+        self.lock.signal()
         
 #    def drawText(self, string):
 #        global text
@@ -69,13 +207,19 @@ class GUI:
 #		comment_text.add(self.comment_list[comment_selected])
 	
     def notifyOfNewLocation(self, location_name):
-		if self.notifyable == 1:
-			appuifw.note(unicode("You are getting close to " + location_name), 'info')
-			self.drawLocationList()
+        if self.notifyable == 1:
+            appuifw.note(unicode("You are getting close to " + location_name), 'info')
+            self.redraw_flag = 1
+            self.lock.signal()
+            #self.drawLocationList()
 	
     def drawBookmarkedLocationList(self):
         self.notifyable = 0
-        appuifw.app.exit_key_handler = self.drawLocationList
+        self.redraw_flag = 1
+        self.gui_state_stack.append(self.gui_state)
+        self.gui_state = "Bookmarks"
+        
+        appuifw.app.exit_key_handler = self.goBack()
         appuifw2.app.exit_key_text = u"Back"        
         tmp = self.location_store.getBookmarkedLocationsList()
         if len(tmp['list']) > 0:
@@ -84,11 +228,11 @@ class GUI:
             self.bookmarked_listbox = appuifw.Listbox(
                 bookmarked_list,
                 self.bookmarkedListCallback
-            )
-            appuifw.app.body = self.bookmarked_listbox
-            appuifw.app.menu = [(u"Delete", self.deleteBookmark), (u"Select", self.bookmarkedListCallback)]
+            )  
         else:
             appuifw.note(u"No bookmarked locations")
+            self.gui_state = self.gui_state_stack.pop()
+        self.lock.signal()
             
 #    def deleteBookmark(self):
 #        appuifw.note(u"delete bookmark")
@@ -211,10 +355,10 @@ class GUI:
         appuifw.note(u"search for locations")
         
     def inputMode(self):
-        appuifw.note(u"input mode won't be implemented this time. Only Key input allowed")
+        appuifw.note(u"input mode isn't implemented at this time. Only Key input allowed")
     
     def deleteBookmark(self):
-        appuifw.note(u"input mode won't be implemented this time. Only Key input allowed")
+        appuifw.note(u"bookmarking isn't implemented at this time. Only Key input allowed")
     
     def fontSize(self):
         appuifw.note(u"Font size option will be available soon")
@@ -251,36 +395,6 @@ class GUI:
         else:
             appuifw.app.body = appuifw.Text( unicode("Searching ...") )
             
-            
-    def locationListCallback(self):
-        self.notifyable = 0
-        appuifw.app.exit_key_handler = self.drawLocationList
-        appuifw2.app.exit_key_text = u"Back"
-        appuifw.app.menu = [(u"Navigate", (
-                                (u"Where am I?", self.whereAmI),
-                                (u"Nearby", self.nearby),
-                                (u"Search", self.search))
-                            ), 
-                            (u"Bookmarked Locations", self.drawBookmarkedLocationList),
-                            (u"Hidden Locations", self.drawHiddenLocationList),
-                            (u"Settings", (
-                                (u"Input Mode", self.inputMode),
-                                (u"Font Size", self.fontSize))
-                            )]
-        
-        if self.current_tpid == None:
-            self.current_tpid = self.location_listbox_mapping[self.location_listbox.current()]
-        
-        #self.drawLocationMenu()
-        tmp = self.location_cache.getLocationMenuList(self.current_tpid)
-        self.location_menu_list = tmp['list']
-        self.location_menu_mapping = tmp['mapping']
-        
-        self.location_menu_listbox = appuifw.Listbox(
-            self.location_menu_list,
-            self.locationMenuCallback
-        )
-        appuifw.app.body = self.location_menu_listbox
    
     def drawLocationMenu(self):
         self.notiyfyable = 0
@@ -304,47 +418,8 @@ class GUI:
             self.locationMenuCallback
         )
         appuifw.app.body = self.location_menu_listbox
+    	
     
-    def locationMenuCallback(self):
-        self.notifyable = 0
-        
-        menu_item_nr_selected = self.location_menu_listbox.current()
-        menu_item_text_selected = self.location_menu_mapping[menu_item_nr_selected]
-        appuifw.app.menu = [(u"Navigate", (
-                                (u"Where am I?", self.whereAmI),
-                                (u"Nearby", self.nearby),
-                                (u"Search", self.search))
-                            ), 
-                            (u"Bookmarked Locations", self.drawBookmarkedLocationList),
-                            (u"Hidden Locations", self.drawHiddenLocationList),
-                            (u"Settings", (
-                                (u"Input Mode", self.inputMode),
-                                (u"Font Size", self.fontSize))
-                            )]
-        if menu_item_text_selected == "Comments":
-            self.comment_list = self.location_cache.getLocationCommentsList(self.current_tpid)
-            if self.comment_list != None:
-                appuifw.app.exit_key_handler = self.locationListCallback
-                appuifw2.app.exit_key_text = u"Back"
-                self.comment_listbox = appuifw.Listbox(self.comment_list, self.locationCommentCallback)
-                appuifw.app.body = self.comment_listbox
-            else:
-                appuifw.note(u"There are no comments yet")
-            
-        elif menu_item_text_selected == "Bookmark":
-            msg = self.location_store.bookmark(self.current_tpid, self.location_cache.getLocationName(self.current_tpid))
-            appuifw.note(unicode(msg))
-            
-
-        elif menu_item_text_selected == "Hide":
-            msg = self.location_store.hide(self.current_tpid, self.location_cache.getLocationName(self.current_tpid))
-            appuifw.note(unicode(msg))
-            
-        else:
-            appuifw.app.exit_key_handler = self.locationListCallback
-            appuifw2.app.exit_key_text = u"Back"
-            #appuifw2.app.body = appuifw2.Text_display( menu_item_text_selected )
-            self.drawText( menu_item_text_selected )
     
     def exit(self):
         yesno = appuifw.query(u"Do you want to exit", "query")
